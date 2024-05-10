648910f9b1543ee733ce8545c2d1b6b1587f208e
amanueldemirew <amanuel.demirew@yahoo.com>
2024-05-10 17:24:32 +0300

    secret key

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -1,8 +1,9 @@
 import os
 from .common import *
-import dj_database_url
 
 DEBUG = False
+
+SECRET_KEY = os.environ['SECRET_KEY']
 
 db_from_env = dj_database_url.parse("postgres://storefront_vxz4_user:Nbstcz54DE4GzeeNH4NoP97FI2PgLleh@dpg-cov0qp021fec73c06tkg-a.oregon-postgres.render.com/storefront_vxz4")
 
