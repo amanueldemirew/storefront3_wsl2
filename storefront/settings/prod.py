4fc2281e5868dbe03fe0e6b3b7ec4ee309ee2559
amanueldemirew <amanuel.demirew@yahoo.com>
2024-05-10 17:13:17 +0300

    prd

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -3,8 +3,6 @@ from .common import *
 
 DEBUG = False
 
-SECRET_KEY = os.environ['SECRET_KEY']
-
 db_from_env = dj_database_url.parse("postgres://storefront_vxz4_user:Nbstcz54DE4GzeeNH4NoP97FI2PgLleh@dpg-cov0qp021fec73c06tkg-a.oregon-postgres.render.com/storefront_vxz4")
 
 # Update DATABASES setting with parsed database URL
@@ -12,4 +10,4 @@ DATABASES = {
     'default': db_from_env
 }
 
-ALLOWED_HOSTS = ['storefront3-wsl2.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1','storefront3-wsl2.onrender.com']
